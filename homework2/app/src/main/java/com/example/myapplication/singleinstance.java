ccc37381716ff650c11c395b472bf785a2ca79ea
jieyanglinzilong <57582207+jieyanglinzilong@users.noreply.github.com>
2020-10-10 20:47:08 +0800

    提交

--- homework2/app/src/main/java/com/example/myapplication/singleinstance.java
+++ homework2/app/src/main/java/com/example/myapplication/singleinstance.java
@@ -23,7 +23,7 @@ public class singleinstance extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(singleinstance.this,singleTask.class);
-                //intent.setData(Uri.parse("tel:10000"));
+              
                 startActivity(intent);
             }
         });
